1cbc9534809cb529bddddbd56fcfecbddc349da5
qiang001 <35440329+qiang001@users.noreply.github.com>
2021-10-08 00:50:07 +0800

    0.0

--- manage/vite.config.js
+++ manage/vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/seamless/manage',
+  base: '/seamless/',
   plugins: [vue()],
   resolve: {
     alias: [
