0ebda44dde4d9ad7b94b3a69f2272a4df857a492
qiang001 <35440329+qiang001@users.noreply.github.com>
2021-10-20 22:25:54 +0800

    21

--- manage/src/views/pages/good/category/index.vue
+++ manage/src/views/pages/good/category/index.vue
@@ -14,8 +14,14 @@ export default { name: 'GoodCategory' }
 <script setup>
 import PagePanel from '@/components/PagePanel.vue'
 import { NButton } from 'naive-ui'
-import { ref } from 'vue'
+import { ref,onMounted, onActivated} from 'vue'
 const num = ref(0)
+onMounted(()=>{
+  $message.success('onMounted')
+})
+onActivated(()=>{
+  $message.success('onActivated')
+})
 </script>
 
 <style></style>
