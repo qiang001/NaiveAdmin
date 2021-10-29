095b3a18db21960894ee1aa0a346c3089e1bf710
qiang001 <ntu.zhouqiang@gmail.com>
2021-10-30 06:05:59 +0800

    MyDropdown组件

--- manage/src/components/CommonTable.vue
+++ manage/src/components/CommonTable.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <n-space justify="end">
+    <n-space justify="end" align="center">
       <n-button
         @click="refresh"
         size="small"
@@ -14,43 +14,39 @@
           </n-icon>
         </template>
       </n-button>
-      <n-element class="my-dropdown">
-        <n-button size="small" :text="true" @click="triggerDropdown">
-          <template #icon>
-            <n-icon>
-              <setting-icon />
-            </n-icon>
-          </template>
-        </n-button>
-        <transition name="slide" mode="out-in">
-          <div class="my-dropdown-content" id="myDropdown" v-if="dropdownShow">
-            <n-space>
+      <my-dropdown>
+        <template #icon>
+          <n-icon>
+            <setting-icon />
+          </n-icon>
+        </template>
+        <template #content>
+          <n-space>
+            <n-checkbox
+              v-model:checked="ifCheckAll"
+              :indeterminate="ifCheckSome"
+              @update:checked="(bool) => checkAll(bool)"
+            >
+              <div class="d-flex a-center py">
+                <div>选择全部</div>
+              </div>
+            </n-checkbox>
+          </n-space>
+          <n-divider></n-divider>
+          <div v-for="(item, index) in dropdownOptions" :key="item.key">
+            <n-space align="center">
               <n-checkbox
-                v-model:checked="ifCheckAll"
-                :indeterminate="ifCheckSome"
-                @update:checked="(bool) => checkAll(bool)"
+                v-model:checked="item.checked"
+                @update:checked="(bool) => checkItem(item, bool)"
               >
                 <div class="d-flex a-center py">
-                  <div>选择全部</div>
+                  <div>{{ item.label }}</div>
                 </div>
               </n-checkbox>
             </n-space>
-            <n-divider></n-divider>
-            <div v-for="(item, index) in dropdownOptions" :key="item.key">
-              <n-space align="center">
-                <n-checkbox
-                  v-model:checked="item.checked"
-                  @update:checked="(bool) => checkItem(item, bool)"
-                >
-                  <div class="d-flex a-center py">
-                    <div>{{ item.label }}</div>
-                  </div>
-                </n-checkbox>
-              </n-space>
-            </div>
           </div>
-        </transition>
-      </n-element>
+        </template>
+      </my-dropdown>
     </n-space>
     <n-data-table
       :scroll-x="props.minWidth"
@@ -73,12 +69,12 @@ import {
   NSpace,
   NButton,
   NIcon,
-  NElement,
   NCheckbox,
   NDivider,
 } from 'naive-ui'
 import { RefreshSharp as RefreshIcon } from '@vicons/material'
 import { TextGrammarSettings24Regular as SettingIcon } from '@vicons/fluent'
+import MyDropdown from '@/components/MyDropdown.vue'
 import EmptyBox from '@/components/EmptyBox.vue'
 import type { PropType } from '@vue/runtime-core'
 import { computed, ref } from 'vue'
@@ -134,44 +130,11 @@ const columns = computed(() =>
     dropdownOptions.value.some((o) => o.checked && o.key === c.key)
   )
 )
-const dropdownShow = ref(false)
-const triggerDropdown = () => {
-  dropdownShow.value = !dropdownShow.value
-}
 </script>
 
 <style scoped>
-.my-dropdown {
-  position: relative;
-}
-.my-dropdown-content {
-  position: absolute;
-  z-index: 3;
-  top: 0;
-  right: 24px;
-  border-radius: 2px;
-  padding: 5px 10px;
-  background-color: var(--modal-color);
-  border: 1px solid var(--border-color);
-  transition: all 0.3s ease;
-}
-
 :deep(.n-divider:not(.n-divider--vertical)) {
   margin-top: 6px;
   margin-bottom: 6px;
-}
-.slide-enter-from {
-  opacity: 0;
-  transform: translateX(10px);
-}
-.slide-enter-active {
-  transition: all 0.3s ease;
-}
-.slide-leave-to {
-  opacity: 0;
-  transform: translateX(10px);
-}
-.slide-leave-active {
-  transition: all 0.3s ease;
 }
 </style>
---
+++ manage/src/components/MyDropdown.vue
@@ -0,0 +1,53 @@
+<template>
+  <n-element class="my-dropdown">
+    <n-button size="small" :text="true" @click="triggerDropdown">
+      <template #icon>
+        <slot name="icon"></slot>
+      </template>
+    </n-button>
+    <transition name="slide-left" mode="out-in">
+      <div class="my-dropdown-content" id="myDropdown" v-if="dropdownShow">
+        <slot name="content"></slot>
+      </div>
+    </transition>
+  </n-element>
+</template>
+
+<script setup lang="ts">
+import { NElement, NButton } from 'naive-ui'
+import { ref } from 'vue'
+const dropdownShow = ref(false)
+const triggerDropdown = () => (dropdownShow.value = !dropdownShow.value)
+</script>
+
+<style scoped>
+.my-dropdown {
+  position: relative;
+}
+.my-dropdown-content {
+  position: absolute;
+  z-index: 3;
+  top: 0;
+  right: 24px;
+  border-radius: 2px;
+  padding: 5px 10px;
+  background-color: var(--modal-color);
+  border: 1px solid var(--border-color);
+  transition: all 0.3s ease;
+}
+
+.slide-left-enter-from {
+  opacity: 0;
+  transform: translateX(10px);
+}
+.slide-left-enter-active {
+  transition: all 0.3s ease;
+}
+.slide-left-leave-to {
+  opacity: 0;
+  transform: translateX(10px);
+}
+.slide-left-leave-active {
+  transition: all 0.3s ease;
+}
+</style>
