92f03eb461188ac5fbd7a2e25775ff166c9cfdbb
qiang001 <35440329+qiang001@users.noreply.github.com>
2021-11-10 15:52:22 +0800

    操作记录省略版完整版

--- manage/src/views/pages/system/operation-recording/components/record-list.vue
+++ manage/src/views/pages/system/operation-recording/components/record-list.vue
@@ -66,59 +66,85 @@ function createColumns(): Array<DataTableColumn> {
     },
     {
       title: '数据变更',
+      key: 'changesEllipsis',
+      render(row) {
+        return renderChanges(row as unknown as IRecord)
+      },
+    },
+    {
+      title: '数据变更(完整版)',
       key: 'changes',
       render(row) {
-        return h(NCollapse, null, {
-          default: () => {
-            let { changes } = row as unknown as IRecord
-            return changes.map((change) => {
-              return h(
-                NCollapseItem,
-                { title: `${change.name} ${change.desc}`, name: change._id },
-                {
-                  default: () => {
-                    return h('div', null, {
-                      default: () => {
-                        return change.diffArr.map((item) => {
-                          let { value, added, removed } = item
-                          let style: any = { whiteSpace: 'pre-wrap' }
-                          let text = value
-                          if (added) {
-                            style.backgroundColor = !store.state.ifDark
-                              ? '#eeffef'
-                              : '#0a440a'
-                            style.color = !store.state.ifDark
-                              ? '#12961a'
-                              : '#72b876'
-                          }
-                          if (removed) {
-                            style.backgroundColor = !store.state.ifDark
-                              ? '#ffeaea'
-                              : '#5c0505b5'
-                            style.color = !store.state.ifDark
-                              ? '#ef2626'
-                              : '#b66e6e'
-                          }
-                          return h(
-                            'div',
-                            { style },
-                            {
-                              default: () => {
-                                return text
-                              },
-                            }
-                          )
-                        })
-                      },
-                    })
-                  },
-                }
-              )
-            })
-          },
-        })
+        return renderChanges(row as unknown as IRecord, false)
       },
     },
   ]
+}
+
+function renderChanges(row: IRecord, ellipsis: boolean = true) {
+  return h(NCollapse, null, {
+    default: () => {
+      let { changes } = row
+      return changes.map((change) => {
+        return h(
+          NCollapseItem,
+          { title: `${change.name} ${change.desc}`, name: change._id },
+          {
+            default: () => {
+              return h('div', null, {
+                default: () => {
+                  return change.diffArr.map((item) => {
+                    let { value, added, removed } = item
+                    let style: any = { whiteSpace: 'pre-wrap' }
+                    let text = value
+                    if (added) {
+                      style.backgroundColor = !store.state.ifDark
+                        ? '#eeffef'
+                        : '#0a440a'
+                      style.color = !store.state.ifDark ? '#12961a' : '#72b876'
+                    }
+                    if (removed) {
+                      style.backgroundColor = !store.state.ifDark
+                        ? '#ffeaea'
+                        : '#5c0505b5'
+                      style.color = !store.state.ifDark ? '#ef2626' : '#b66e6e'
+                    }
+                    if (ellipsis && !added && !removed && value.length > 30) {
+                      text = useEllipsis(value, 30)
+                    }
+                    return h(
+                      'div',
+                      { style },
+                      {
+                        default: () => {
+                          return text
+                        },
+                      }
+                    )
+                  })
+                },
+              })
+            },
+          }
+        )
+      })
+    },
+  })
+}
+
+function useEllipsis(value: string, targetLength: number) {
+  let show = value.substring(0, targetLength)
+  let hide = value.substring(targetLength)
+  let lines = hide.split('\n').length - 1
+  return (
+    show +
+    (show[targetLength - 1].trim()
+      ? lines > 0
+        ? `...\n... [${lines} lines] ...`
+        : '...'
+      : lines > 0
+      ? `... [${lines} lines] ...`
+      : '')
+  )
 }
 </script>
