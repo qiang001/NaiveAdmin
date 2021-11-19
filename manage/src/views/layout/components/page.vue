053947a560f394d85fcbf94bb72c67b5fd4df58f
qiang001 <35440329+qiang001@users.noreply.github.com>
2021-11-19 15:21:52 +0800

    页面改为横向切入

--- manage/src/views/layout/components/page.vue
+++ manage/src/views/layout/components/page.vue
@@ -17,14 +17,14 @@ const store = useStore()
 /* route transitions */
 .route-enter-from {
   opacity: 0;
-  transform: translateY(-30px);
+  transform: translateX(-30px);
 }
 .route-enter-active {
   transition: all 0.3s ease;
 }
 .route-leave-to {
   opacity: 0;
-  transform: translateY(-30px);
+  transform: translateX(30px);
 }
 .route-leave-active {
   transition: all 0.3s ease;
